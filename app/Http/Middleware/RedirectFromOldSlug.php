e99972a3c545c991d3694cae5b8dfb96482f3a9b
KlaysKAS <70439034+KlaysKAS@users.noreply.github.com>
2021-11-13 05:34:17 +1000

    Lesson 7: Add redirecting middleware (#7)
    * Lesson 7: Add redirecting middleware
    
    * Lesson 7: Fix url takes in ChangeNewsSlug and RedirectFromOldSlug, and add check in ChangeNewsSlug
    
    * Lesson 7: Fix parameters and compare slugs
    
    * Lesson 7: Correcting the existence of a record in redirectDB

---
+++ app/Http/Middleware/RedirectFromOldSlug.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Redirect;
+use Closure;
+use Illuminate\Http\Request;
+
+class RedirectFromOldSlug
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $url = parse_url($request->url(), PHP_URL_PATH);
+
+        $redirect = Redirect::where('old_slug', $url)->orderByDesc('created_at')->orderByDesc('id')->first();
+        $redirectTo = null;
+
+        while ($redirect !== null) {
+            $redirectTo = $redirect;
+            $redirect = Redirect::where('old_slug', $redirect->new_slug)
+                ->orderByDesc('created_at')
+                ->orderByDesc('id')
+                ->first();
+        }
+
+        if ($redirectTo !== null) {
+            return redirect($redirectTo->new_slug);
+        }
+
+        return $next($request);
+    }
+}
